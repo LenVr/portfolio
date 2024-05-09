3c3169eec2f1ec2934ffdb4ad206ae796d47b14a
Len <villalobosrojas22@gmail.com>
2024-05-09 13:18:14 -0600

    fade creation

--- Public/js/controllers/projects.js
+++ Public/js/controllers/projects.js
@@ -1,13 +1,39 @@
 import { showProject } from "../index.js";
-import { WANO } from "../libs/constantes.js";
+import { ASSISTANT, FACTURATION, MEMORY, PINPONG, WANO } from "../libs/constantes.js";
+import { div } from "../libs/html.js";
 
 const wano = document.getElementById('prj1');
 const pingPong = document.getElementById('prj2');
 const memory = document.getElementById('prj3');
 const facturation = document.getElementById('prj4');
 const assistant = document.getElementById('prj5');
+const modalContainer = document.getElementById('modalContainer')
+
+
 
 wano.addEventListener('click', () => {
-    console.log('wanoclickFct')
     showProject(WANO);
-})
+});
+
+pingPong.addEventListener('click', () => {
+    showProject(PINPONG);
+});
+
+memory.addEventListener('click', () => {
+    showProject(MEMORY);
+});
+
+facturation.addEventListener('click', () => {
+    showProject(FACTURATION);
+});
+
+assistant.addEventListener('click', () => {
+    showProject(ASSISTANT);
+});
+
+export function showWanoProject() {
+
+    const fade = div(modalContainer, { className: 'fade' });
+    const elementContainer = div(fade, { className: 'elementContainer' });
+
+}
--- Public/js/index.js
+++ Public/js/index.js
@@ -1,3 +1,4 @@
+import { showWanoProject } from "./controllers/projects.js";
 import { ASSISTANT, FACTURATION, MEMORY, PINPONG, WANO } from "./libs/constantes.js";
 
 
@@ -5,18 +6,23 @@ export function showProject(type) {
     switch (type) {
         case WANO:
             console.log('wanito')
+            showWanoProject();
             break;
         case PINPONG:
-
+            console.log('ping')
+            showPingPongProject();
             break;
         case MEMORY:
-
+            console.log('memory')
+            showMemoryGame();
             break;
         case FACTURATION:
-
+            console.log('facturation')
+            showFacturactionProject()
             break;
         case ASSISTANT:
-
+            console.log('assist')
+            showAssistantProject()
             break;
         default:
             break;
