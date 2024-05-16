8d07654523c78aaa48602e8f8ee83db5664644a1
Len <villalobosrojas22@gmail.com>
2024-05-16 15:16:15 -0600

    contact commit

--- Public/js/controllers/contactMe.js
+++ Public/js/controllers/contactMe.js
@@ -9,6 +9,10 @@ contactbtn.addEventListener('click', () => {
     showProject(CONTACT);
 })
 
+sendMeMessage.addEventListener('click', () => {
+    window.open('https://walink.co/768291', '_blank');
+})
+
 export function showContactForm() {
 
     const fade = div(document.body, { className: 'fadeContainer' });
@@ -21,6 +25,24 @@ export function showContactForm() {
         fade.removeChild(elementContainer);
         fade.removeChild(equis);
     });
+    const email = 'lenzvinsmoke@gmail.com'
+
+    contactFormContainer.innerHTML = `
+        <form action="https://formsubmit.co/${email}" method="POST">
+            <p>Full name</p>
+            <input id="name" type="text" required>
+            <p>Company</p>
+            <input id="company" type="text">
+            <p>Phone number</p>
+            <input id="phone" type="tel" required>
+            <p>Email</p>
+            <input id="email" type="tel" required>
+            <br>
+            <button type="submit" id="sendForm" required>
+                Send message
+            </button>
+        </form>
+    `
 }
 
 
