686b4fff72387df531a9962d937d87a32e64251c
Len <villalobosrojas22@gmail.com>
2024-09-27 21:45:38 -0600

    deleting old files

--- Public/js/libs/constantes.js
+++
@@ -1,8 +0,0 @@
-
-export const WANO = 1;
-export const PINPONG = 2;
-export const MEMORY = 3;
-export const FACTURATION = 4;
-export const ASSISTANT = 5;
-export const CONTACT = 6
-export const CLOSE = 7;
