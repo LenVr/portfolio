180caf6895670db23aed693fcfb6a51fff1a22f0
Len <villalobosrojas22@gmail.com>
2024-10-05 17:36:16 -0600

    img added

--- Public/js/controller/portfolioController/portfolioView.js
+++ Public/js/controller/portfolioController/portfolioView.js
@@ -1,5 +1,5 @@
 import { LEFT, RIGHT } from "../../libraries/constants.js";
-import { div, p } from "../../libraries/html.js";
+import { div, img, p } from "../../libraries/html.js";
 import { ViewEnhanced } from "../../view/viewEnhanced.js";
 import { CharacterController } from "../character/characterController.js";
 
@@ -11,18 +11,23 @@ export class PortfolioView extends ViewEnhanced {
         this.character = new CharacterController(this, this.container);
         this.scrollAmount = 25;
 
-        this.leftButton = div(this.container, { className: 'leftButton', onmousedown: this.onLeftButton.bind(this), onmouseup: this.upLeftButton.bind(this) })
-        this.rightButton = div(this.container, { className: 'rightButton', onmousedown: this.onRightButton.bind(this), onmouseup: this.upRightButton.bind(this) })
-
         this.welcomeContainer = div(this.container, { className: 'welcomeContainer' })
         this.nameContainer = div(this.welcomeContainer, { className: 'nameContainer' })
         this.welcomeTitle = p(this.nameContainer, { className: 'welcomeTitle' });
         this.instructions = p(this.welcomeContainer, { className: 'instructions' });
         this.instructionsMobile = p(this.welcomeContainer, { className: 'instructionsMobile' });
 
-        this.personalContainer = div(this.container, { className: 'personalContainer' });
-        this.welcomeMsg = div(this.personalContainer, { className: 'welcomeMsg', textContent: 'Welcome' })
-        this.aboutInformation = p(this.personalContainer, { className: 'aboutInformation', innerHTML: `I'm a passionate web developer based in Costa Rica <br> Someone whole LOVES technology and science, I also love <br> One Piece and Bikes, but I won't tell you my secrets` })
+        this.aboutMeContainer = div(this.container, { className: 'aboutMeContainer' });
+        this.aboutMeImgContainer = div(this.aboutMeContainer, { className: 'aboutMeImgContainer' });
+        this.aboutMeImg = img(this.aboutMeContainer, { className: 'aboutMeImg', src: '/public/assets/meReal.png' })
+        this.aboutMeTextContainer = div(this.aboutMeContainer, { className: 'aboutMeTextContainer' })
+        this.welcomeMsg = div(this.aboutMeTextContainer, { className: 'welcomeMsg', textContent: 'Welcome' });
+        this.aboutInformation = p(this.aboutMeTextContainer, { className: 'aboutInformation', innerHTML: `I'm a passionate web developer based in Costa Rica <br> Someone whole LOVES videogames, technology and science, I also <br> love One Piece and Bikes, but I won't tell you my secrets` });
+
+
+        this.leftButton = div(this.container, { className: 'leftButton', onmousedown: this.onLeftButton.bind(this), onmouseup: this.upLeftButton.bind(this) })
+        this.rightButton = div(this.container, { className: 'rightButton', onmousedown: this.onRightButton.bind(this), onmouseup: this.upRightButton.bind(this) })
+
 
         this.writing = true;
         this.index = 0
@@ -50,7 +55,7 @@ export class PortfolioView extends ViewEnhanced {
             this.welcomeTitle.textContent = '';
             this.index = 0;
             this.writing = true;
-            this.instructions.innerHTML = 'Press <kbd>A</kbd> and <kbd>D</kbd> to move';
+            this.instructions.innerHTML = 'Press <kbd>A</kbd> or <kbd>D</kbd> to move';
             this.instructionsMobile.innerHTML = 'If you are in a mobile, touch the border left or right'
         }
     }
